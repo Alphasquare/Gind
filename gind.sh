5f6551e0e5475a3f2f6920f5196ac254eca4dbe6
Sergio Diaz <Alphapixels@users.noreply.github.com>
2015-02-28 17:39:38 -0400

    gind: added "all" search type

--- gind.sh
+++ gind.sh
@@ -9,7 +9,7 @@ fi
 
 if [ -z "$2" ]
  then
- echo "No file name supplied as argument."
+ echo "No pattern supplied as argument."
  echo ""
  echo "syntax: gind <type> <pattern...>"
   exit 1
@@ -22,5 +22,16 @@ fi
 
 if [ $1 == "file" ] 
 	then
+	git ls-files | grep $2
+fi
+
+if [ $1 == "all" ] 
+	then
+	echo "Strings: "
+	echo ""
+	git grep $2
+	echo ""
+	echo "Files: "
+	echo ""
 	git ls-files | grep $2
 fi
